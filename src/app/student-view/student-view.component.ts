f8ad25a3014366f2219bc1b219d53b3550483683
Kevin Kusuma <kevinkusuma@gatech.edu>
2018-11-01 15:58:54 -0400

    Added print export to pdf for printing student view report.

--- src/app/student-view/student-view.component.ts
+++ src/app/student-view/student-view.component.ts
@@ -98,6 +98,7 @@ export class StudentViewComponent implements OnInit {
     let dialogRef = this.matDialog.open(ExportDialogComponent, {
       width: '250px'
     });
+    window.print();
   }
 }
 
