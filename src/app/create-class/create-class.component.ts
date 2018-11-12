7946a5e4dec91b88fcf1f2ac0ae5621a3c15478b
Matthew Lemons <lemonsmatt13@gmail.com>
2018-11-11 21:38:42 -0500

    added reading from csv

--- src/app/create-class/create-class.component.ts
+++ src/app/create-class/create-class.component.ts
@@ -81,9 +81,26 @@ export class CreateClassComponent implements OnInit {
     //doNothing
   }
 
-  addStudentsFromFile() : void {
-    //doNothing
-  }
+changeListener(files: FileList){
+  console.log(files);
+  if(files && files.length > 0) {
+     let file : File = files.item(0); 
+       console.log(file.name);
+       console.log(file.size);
+       console.log(file.type);
+       let reader: FileReader = new FileReader();
+       reader.readAsText(file);
+       reader.onload = (e) => {
+          let csv: string = reader.result;
+          let values = csv.split(',');
+          for (let i = 0; i <values.length/2; i++)
+          {
+            console.log(values[i], values[i+1]);
+            this.addStudent(values[i], values[i+1])
+          }
+       }
+    }
+}
 
   getErrorMessage() {
     return this.courseNUmValidation.hasError('required') ? 'Field Cannot Be Empty' :
