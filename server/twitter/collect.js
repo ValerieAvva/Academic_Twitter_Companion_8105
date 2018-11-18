3b4600a5a56dfa8b9dbc398823ab864e9214201c
Nicole Cantrell <nikkicantrell@Nicoles-MacBook-Air.local>
2018-11-18 12:54:42 -0500

    Adding comments to twitter scraper

--- server/twitter/collect.js
+++ server/twitter/collect.js
@@ -1,8 +1,9 @@
-//database
+//database connection
 var mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://matt:msl@academictwitter-nlieo.mongodb.net/test?retryWrites=true').then(() => {
     console.log('connected');
 }).catch(err => console.log(err));
+//database models
 var TweetModel = require("../models/tweet.js");
 var SectionModel = require("../models/section.js");
 
@@ -11,6 +12,8 @@ var Twitter = require('twitter');
 var config = require('./config.js');
 var request = require('request');
 var T = new Twitter(config);
+
+//find all of the hashtags in the database
 SectionModel.find({}, 'topics', function(err, hashtags) {
     if(!err) {
         getMaxIds(0, hashtags);
@@ -18,6 +21,8 @@ SectionModel.find({}, 'topics', function(err, hashtags) {
         console.log(err);
     }
 });
+
+//get the last (max) tweet id in the database for each hashtag
 var hashtagsQuery = [];
 var getMaxIds = function(i, hashtagsArr) {
     if (i < hashtagsArr.length) {
@@ -40,12 +45,19 @@ var getMaxIds = function(i, hashtagsArr) {
         getTweets(hashtagsQuery);
     }
 };
+
+//helper function to get the tweet with the max id
 async function getTweet(currHash) {
     const tweet = await TweetModel.find({hashtags: currHash}).sort({id: -1}).select('id').limit(1);
     return tweet;
 }
+
+//get all the tweets since the max id in the database
 function getTweets(hashtagIds) {
 var hashIndex = 0;
+//use async.whilst because we need to wait for each get function to complete running before running the next
+//this is because the twitter API returns tweets in "pages" and allows you to set max_id to the last id in the
+//previous query in order to get the next page
 async.whilst(function() {
     return hashIndex < hashtagIds.length;
 },
@@ -55,6 +67,7 @@ function(nextouter) {
     var params;
     var done = 0;
     var query = "#" + hashtagIds[hashIndex]['hashtag'];
+    //if there are tweets in the database for this hashtag, only get the tweets after the greatest id
     if (lastid > 0) {
         params = { q: query, count: 100, tweet_mode: "extended", since_id: lastid };
     } else {
@@ -65,11 +78,14 @@ function(nextouter) {
             var tweets = data['statuses'];
             for(let t of tweets)
             {
+                //get all of the hashtags in this tweet for the "hashtags" column in the tweet model
                 var hashtags = [];
                 for (let h of t.entities.hashtags)
                 {
                     hashtags.push(h.text)
                 }
+
+                //put each tweet in the database
                 var newTweet = TweetModel({
                     id: t.id,
                     username: t.user.name,
@@ -88,11 +104,13 @@ function(nextouter) {
                 maxId = t.id
             }
             if(tweets.length > 0) {
+                //save the last id from the results to pass as a query parameter to get the next page
                 maxId = tweets[tweets.length - 1]['id'];
                 async.whilst(function() {
                     return maxId > lastid || done == 1;
                 },
                 function (next) {    
+                    //the query parameters now include the max_id 
                     if (lastid > 0) {
                         params = { q: query, count: 100, since_id: lastid, max_id: maxId, tweet_mode: "extended" };
                     } else {
