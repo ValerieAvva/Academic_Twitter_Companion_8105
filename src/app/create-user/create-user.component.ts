72e9243434b335bf2d78bdd3fcf11c5e2a8cb010
Valerie Avva <vavva3@gatech.edu>
2018-11-27 19:07:22 -0500

    base level to login UI

---
+++ src/app/create-user/create-user.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent implements OnInit {
+  hide = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  createUser() {
+    // TODO: Fill in
+  }
+
+}
