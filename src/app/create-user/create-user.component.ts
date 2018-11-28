648651b59b5c2a24a83468f5cfa0460e256bb47f
Valerie Avva <vavva3@gatech.edu>
2018-11-28 10:15:23 -0500

    added 2nd password confirmation

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-create-user',
@@ -8,7 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class CreateUserComponent implements OnInit {
   hide = true;
 
-  constructor() { }
+  registrationFormGroup: FormGroup;
+  passwordFormGroup: FormGroup;
+ 
+  constructor(private formBuilder: FormBuilder) {
+    this.passwordFormGroup = this.formBuilder.group({
+      password: ['', Validators.required],
+      repeatPassword: ['', Validators.required]
+    }, {
+      validator: RegistrationValidator.validate.bind(this)
+    });
+    this.registrationFormGroup = this.formBuilder.group({
+      username: ['', Validators.required],
+      passwordFormGroup: this.passwordFormGroup
+    });
+   }
 
   ngOnInit() {
   }
@@ -17,4 +33,24 @@ export class CreateUserComponent implements OnInit {
     // TODO: Fill in
   }
 
+}
+
+export class RegistrationValidator {
+  static validate(registrationFormGroup: FormGroup) {
+      let password = registrationFormGroup.controls.password.value;
+      let repeatPassword = registrationFormGroup.controls.repeatPassword.value;
+
+      if (repeatPassword.length <= 0) {
+          return null;
+      }
+
+      if (repeatPassword !== password) {
+          return {
+              doesMatchPassword: true
+          };
+      }
+
+      return null;
+
+  }
 }
